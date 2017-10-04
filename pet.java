50c45b0ecc7c1f076e169b785f926a7548b8744f
acr24 <acr24@njit.edu>
2017-10-04 02:00:32 -0400

    Add files via upload

---
+++ pet.java
@@ -0,0 +1,16 @@
+class Pet
+{
+   private String petType,petName,rOV;
+   Pet(String type,Sting name,String visit)
+   {
+      petType=type;
+      petName=name;
+      rOV=visit;
+    }
+    public String toString()
+    {
+      return petName + "is a "+petType+", and is visiting due to "+rOV;
+     }
+                                                                                                                  
+}
+      
